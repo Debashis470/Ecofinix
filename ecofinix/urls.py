d7ea9b63f00ecf410dbb01416f0c42b372a089d8
Debashis470 <debasisdhir7@gmail.com>
2026-04-23 16:40:59 +0530

    Refactor imports and clean up urlpatterns

--- ecofinix/urls.py
+++ ecofinix/urls.py
@@ -15,22 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
-from savings import views
+from django.urls import path, include
 
-
-from finance import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('finance.urls')),
     path('tax/', include('tax.urls')),
     path('savings/', include('savings.urls')),
-   path('investment/', include('investment.urls')),
-   path("savings/", views.savings_view, name="savings"),
-   
-   
-
-
-    
+    path('investment/', include('investment.urls')),
 ]
