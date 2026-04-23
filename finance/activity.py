2ba58f115eecd3f65be5a3be2d22ed49ebbac9d3
Debashis470 <debasisdhir7@gmail.com>
2026-04-23 16:52:36 +0530

    Delete finance directory

--- finance/activity.py
+++
@@ -1,8 +0,0 @@
-from .models import UserActivity
-
-def log_activity(request, action_text):
-    if request.user.is_authenticated:
-        UserActivity.objects.create(
-            user=request.user,
-            action=action_text
-        )
