a94ebd896b7a10c8098b018a43a374e794824640
Debashis470 <debasisdhir7@gmail.com>
2026-04-24 09:36:21 +0530

    Update urls.py

--- ecofinix/urls.py
+++ ecofinix/urls.py
@@ -18,9 +18,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('finance.urls')),
-    path('tax/', include('tax.urls')),
-    path('savings/', include('savings.urls')),
-    path('investment/', include('investment.urls')),
+    path("admin/", admin.site.urls),
+    path("", include("finance.urls")),
+    path("tax/", include("tax.urls")),
+    path("savings/", include("savings.urls")),
+    path("investment/", include("investment.urls")),
 ]
