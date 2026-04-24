bdb10060e66f32cdb386b9ff98de8f797a3f337d
Debashis470 <debasisdhir7@gmail.com>
2026-04-24 08:58:23 +0530

    Update settings.py

--- ecofinix/settings.py
+++ ecofinix/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
 # -----------------------------
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -114,11 +116,13 @@ USE_TZ = True
 # -----------------------------
 # Static files
 # -----------------------------
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
-    BASE_DIR / "ecofinix" / "static",  # global static folder
+    BASE_DIR / "ecofinix" / "static",
 ]
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 # -----------------------------
 # Default primary key field
 # -----------------------------
